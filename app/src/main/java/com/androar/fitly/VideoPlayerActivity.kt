8207bcc77b2c85180749b4a81b34651a8bd0ab0e
techydevil <techydevil@gmail.com>
2020-04-16 22:37:48 +0530

    Bottom Sheet with recyclerview

--- app/src/main/java/com/androar/fitly/VideoPlayerActivity.kt
+++ app/src/main/java/com/androar/fitly/VideoPlayerActivity.kt
@@ -1,10 +1,18 @@
 package com.androar.fitly
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.*
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.master.exoplayer.MasterExoPlayerHelper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class VideoPlayerActivity : AppCompatActivity() {
@@ -14,10 +22,8 @@ class VideoPlayerActivity : AppCompatActivity() {
         setContentView(com.androar.fitly.R.layout.activity_video_player)
 
         var videosList : ArrayList<String> = arrayListOf()
-        videosList.add("https://v16.muscdn.com/7f1bb148f117fe41ca0284ed9ea3b22f/5e9863c7/video/tos/useast2a/tos-useast2a-ve-0068c001/dc1a2ba223a647909142686f019a834b/?a=1233&br=1276&bt=638&cr=0&cs=0&dr=0&ds=6&er=&l=2020041607550601018907203435183787&lr=musically&qs=5&rc=M25rOHZ1c2ZldDMzZDczM0ApNDc8Ojs7ZTw8NzRmNGlmNmdxMnNqZmNkcl9fLS01MTZzcy5jYjY2LWEtNjNfYTM1YDI6Yw%3D%3D&vl=&vr=")
-        videosList.add("https://v19.muscdn.com/25fb825a6ffe35c82b29bcd5a5034bec/5e9868aa/video/tos/useast2a/tos-useast2a-ve-0068c003/698ddce4253349ac9f548f5cc014807b/?a=1233&br=1544&bt=772&cr=0&cs=0&dr=0&ds=6&er=&l=202004160815110101890740383C1764A6&lr=musically&qs=5&rc=M210ZGpuMzV0dDMzaDczM0ApaTo3aDdnN2VkN2Y5OjRnaGc1Ni1fX19qXjVfLS01MTZzc2JfYS9fYS9jXi1eXi9hMTA6Yw%3D%3D&vl=&vr=")
-        videosList.add("https://v16.muscdn.com/05d12749aa3c580bba9c2fea12e7991b/5e986893/video/tos/useast2a/tos-useast2a-pve-0068/f16280d55b3148e1b8bd1ccc904047cb/?a=1233&br=1598&bt=799&cr=0&cs=0&dr=0&ds=6&er=&l=20200416081537010189066018531B219D&lr=musically&qs=5&rc=M215cXZnPHQ6dDMzNTczM0ApaGVoaDc5M2Q6NzQ1ZDs5OWdeaS9mNS5uLS1fLS0wMTZzcy40LjYvLTJjLS01Y2FhMTU6Yw%3D%3D&vl=&vr=")
-        videosList.add("https://v16.muscdn.com/acc360e71ea840ad5d3fd6d177a2aed1/5e9868bc/video/tos/useast2a/tos-useast2a-ve-0068c003/d9f41df7223e4192a3d6560e96078f3a/?a=1233&br=1322&bt=661&cr=0&cs=0&dr=0&ds=6&er=&l=202004160815430101890740363F140739&lr=musically&qs=5&rc=MzZzNzxrb3JzdDMzOzczM0ApZzdnO2k1NmU2N2QzaDloNmdwMG1icHFkLy5fLS02MTZzczMwMTAtMWNiMmEyNF9hYmE6Yw%3D%3D&vl=&vr=")
+        videosList.add("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+        videosList.add("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
 
         val recyclerView = findViewById<RecyclerView>(R.id.rvVideos)
         val linearLayoutManager = LinearLayoutManager(this)
@@ -39,7 +45,62 @@ class VideoPlayerActivity : AppCompatActivity() {
         val adapter = RecyclerVideoAdapter(videosList)
         recyclerView.adapter = adapter
 
+        loadDummyData()
+        populateActivities()
+    }
 
+    fun loadDummyData() {
+        val recyclerView = findViewById(R.id.rvExcercises) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        val arrayList = ArrayList<RecyclerItemActivities>()
+        arrayList.add(RecyclerItemActivities("", "", "", "", "", ""))
+        arrayList.add(RecyclerItemActivities("", "", "", "", "", ""))
+        arrayList.add(RecyclerItemActivities("", "", "", "", "", ""))
+        val adapter = RecyclerItemActivitesAdapter(arrayList)
+        recyclerView.adapter = adapter
+    }
+    private fun populateActivities() {
+        val recyclerView = findViewById(R.id.rvExcercises) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+
+
+        val arrayList = ArrayList<RecyclerItemActivities>()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://goforbg.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(Api::class.java)
+        val call = api.getActivities()
+        call.enqueue(object : Callback<List<RecyclerItemActivities>> {
+            override fun onResponse(
+                call: Call<List<RecyclerItemActivities>>,
+                response: Response<List<RecyclerItemActivities>>
+            ) {
+                val activitesList = response.body()!!
+                for (i in activitesList.indices) {
+                    arrayList.add(
+                        RecyclerItemActivities(
+                            activitesList[i].n,
+                            activitesList[i].t,
+                            activitesList[i].i,
+                            activitesList[i].e,
+                            activitesList[i].v,
+                            activitesList[i].d
+                        )
+                    )
+                }
+                val adapter = RecyclerItemActivitesAdapter(arrayList)
+                recyclerView.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<RecyclerItemActivities>>, t: Throwable) {
+                Log.d("RetrofitTest", t.toString())
+
+                Toast.makeText(applicationContext, "Your internet is moo!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
 
     }
+
 }
